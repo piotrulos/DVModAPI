ef9186a427863fb3a03326e563f8c3b4d68dc27f
piotrulos <piotr.wwww@gmail.com>
2020-09-24 17:52:48 +0200

    DV Mod API Test

---
+++ DVModApi/DVMODCheckLoading.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DVModApi
+{
+    class DVMODCheckLoading : MonoBehaviour
+    {
+        public DVApi_LoadingScreen loadingScreenEvents;
+
+        //When This Object is destroyed, game finished loading
+        void OnDestroy()
+        {
+            loadingScreenEvents.LoadingScreenFinished();
+        }
+    }
+}
---
+++ DVModApi/DVModAPI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace DVModApi
+{
+    public delegate void DVisLoaded();  // delegate
+    public static class DVModAPI
+    {
+        public static DVApi_LoadingScreen DVLoadingScreen;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            DVLoadingScreen = new DVApi_LoadingScreen();
+            GameObject init = new GameObject("DVModApi - init");
+            init.AddComponent<DVMODInit>().loadingScreenEvents = DVLoadingScreen;
+            GameObject.DontDestroyOnLoad(init);
+            DVLoadingScreen.loadingScreenFinished += DVLoadingScreen_loadingScreenFinished;
+            return true;
+        }
+
+        private static void DVLoadingScreen_loadingScreenFinished()
+        {
+            Debug.Log("<color=cyan>[DVModAPI] Game Loaded</color>");
+
+        }
+    }
+
+    public class DVApi_LoadingScreen
+    {
+        public event DVisLoaded loadingScreenFinished;
+        internal void LoadingScreenFinished()
+        {
+            OnLoadingScreenFinished();
+        }
+        protected virtual void OnLoadingScreenFinished()
+        {
+            loadingScreenFinished?.Invoke();
+        }
+    }
+
+}
