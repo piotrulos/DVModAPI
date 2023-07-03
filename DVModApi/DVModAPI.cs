1a12990d451802d174846b95a8a7eb271e7cb37f
piotrulos <piotr.wwww@gmail.com>
2023-07-03 20:06:49 +0200

    Remake for simulator update

--- DVModApi/DVModAPI.cs
+++ DVModApi/DVModAPI.cs
@@ -1,44 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
+using System;
 using UnityEngine;
 using UnityModManagerNet;
 
 namespace DVModApi
 {
-    public delegate void DVisLoaded();  // delegate
+    public delegate void DVisLoaded(); 
     public static class DVModAPI
     {
-        public static DVApi_LoadingScreen DVLoadingScreen;
-
+        public static DVAPI_LoadingScreen DVLoadingScreen;
+        private static string DVModAPIVersion = "0.1";
         static bool Load(UnityModManager.ModEntry modEntry)
         {
-            DVLoadingScreen = new DVApi_LoadingScreen();
-            GameObject init = new GameObject("DVModApi - init");
-            init.AddComponent<DVMODInit>().loadingScreenEvents = DVLoadingScreen;
+            Debug.Log("<color=cyan>[DVModAPI] Initializing...</color>");
+            DVLoadingScreen = new DVAPI_LoadingScreen();
+            GameObject init = new GameObject("DVModApi");
             GameObject.DontDestroyOnLoad(init);
-            DVLoadingScreen.loadingScreenFinished += DVLoadingScreen_loadingScreenFinished;
+            WorldStreamingInit.LoadingFinished += WorldStreamingInit_LoadingFinished;
+            DVLoadingScreen.LoadingFinished += DVLoadingScreen_LoadingFinished;
+            Debug.Log($"<color=cyan>[DVModAPI] Loaded (version: {DVModAPIVersion})</color>");
             return true;
         }
 
-        private static void DVLoadingScreen_loadingScreenFinished()
+        private static void DVLoadingScreen_LoadingFinished()
         {
             Debug.Log("<color=cyan>[DVModAPI] Game Loaded</color>");
+        }
 
+        private static void WorldStreamingInit_LoadingFinished()
+        {
+            DVLoadingScreen.LoadingScreenFinished();
+        }
+
+               
+        /// <summary>
+        /// Load Asset bundle from your mod folder
+        /// </summary>
+        /// <param name="modEntryPath"></param>
+        /// <param name="bundleName"></param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public static AssetBundle LoadAssetBundle(string modEntryPath, string bundleName)
+        {
+            string bundle = Path.Combine(modEntryPath, bundleName);
+
+            if (File.Exists(bundle))
+            {
+                Debug.Log($"Loading AssetBundle: {bundle}...");
+                return AssetBundle.LoadFromMemory(File.ReadAllBytes(bundle));
+            }
+            else
+            {
+                throw new FileNotFoundException($"Error: File not found: {bundle} {Environment.NewLine}", bundle);
+            }
         }
     }
 
-    public class DVApi_LoadingScreen
+    public class DVAPI_LoadingScreen
     {
-        public event DVisLoaded loadingScreenFinished;
+        public event DVisLoaded LoadingFinished;
         internal void LoadingScreenFinished()
         {
             OnLoadingScreenFinished();
         }
         protected virtual void OnLoadingScreenFinished()
         {
-            loadingScreenFinished?.Invoke();
+            LoadingFinished?.Invoke();
         }
     }
 
